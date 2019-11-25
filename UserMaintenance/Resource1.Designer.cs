3328d2ce0b36b009c7a1f5994a8715ece80df79f
Simon <geri.simon97@gmail.com>
2019-11-25 18:52:43 +0100

    eight_commit

--- UserMaintenance/Resource1.Designer.cs
+++ UserMaintenance/Resource1.Designer.cs
@@ -70,7 +70,7 @@ namespace UserMaintenance {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Teljes név.
+        ///   Looks up a localized string similar to Keresztnév.
         /// </summary>
         internal static string FullName {
             get {
